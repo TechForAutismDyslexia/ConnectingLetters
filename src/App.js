bad1bedf99e2cb9ea94036ac472889949870e0e0
deepak_p <deepakpottavatri06@gmail.com>
2024-06-03 16:02:05 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@
 import Game from './components/Game';
 import Game3 from './components/Game3';
 import { BrowserRouter as Router , Routes , Route, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './index.css';
+import Game3_1 from './components/Game3_1';
 
 function App() {
   const navigate = useNavigate();
@@ -17,15 +19,28 @@ function App() {
         {/* <h1>Form Words using the letters!</h1> */}
         <Routes>
           <Route path="/game3" element={<Game3 />} />
+          <Route path='/game3_1' element={<Game3_1/>} />
           <Route path="/" element={
-             <div className="card" style="width: 18rem;">
+             <>
+             <div className="card" style={{width :"18rem"}}>
              <div class="card-body">
-               <h5 class="card-title">Card title</h5>
-               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-               <a href="#" class="btn btn-primary">Go somewhere</a>
+               <h5 class="card-title">Level 0</h5>
+               <p class="card-text">Connect two letter across threads </p>
+               <Link to="/game3" type='button' className='btn btn-primary'>button</Link>
              </div>
            </div>
+           <div className="card" style={{width :"18rem"}}>
+             <div class="card-body">
+               <h5 class="card-title">Level 0</h5>
+               <p class="card-text">Connect two letter across threads </p>
+               <Link to="/game3_1" type='button' className='btn btn-primary'>button</Link>
+             </div>
+           </div>
+           </>
+           
+
           } />
+
         </Routes>
        
       
