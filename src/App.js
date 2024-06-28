0f7a736cf8769b5b42c50649f869f4c691598803
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2024-06-28 14:31:04 +0530

    Standard template size & format, better var names
    Co-authored-by: Jai-JAP <jai.jap.318@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './index.css';
 import GameThreeOne from './components/GameThreeOne';
+import Game from './components/Game';
  
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           
           <Route path='/game3_1' element={<GameThreeOne/>} />
+          <Route path='/game' element={<Game/>} />
           <Route path="/" element={
             <section className="container">
             <div className="d-flex flex-column justify-content-center align-items-center">
@@ -26,7 +28,8 @@ function App() {
                   <h5 className="card-title">Level 1</h5>
                   <img src="/Dimg.jpg" alt="Display" className="img-fluid rounded-3 border border-4 border-black"  />
                   <p className="card-text" >Connect a letter on Left Side of the page to a letter on right side of the page by following the line.</p>
-                  <Link to="/game3_1" type="button" className="btn btn-primary d-block" onClick={clicked}>Start</Link>
+                  <Link to="/game3_1" type="button" className="btn btn-primary d-block" onClick={clicked}>Game1</Link>
+                  <Link to="/game" type='button' className="btn btn-primary d-block" onClick={clicked}>Game2</Link>
                 </div>
               </div>
             </div>
